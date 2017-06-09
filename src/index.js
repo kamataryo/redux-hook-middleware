c5c1576d6af32f4072c66a118c8c44102f207dd2
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-06-09 14:38:36 +0900

    Add registered hook store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
 // hooking Middleware
-// refer to http://qiita.com/kuy/items/57c6007f3b8a9b267a8e
 
-const prehooks = {}
+export const hooks = {}
 
-const posthooks = {
-  ['hooking action names']: (store, action) => {
-    // doAnything
+/**
+ * register dispatch hook
+ * @param  {'pre'|'post'} position define pre or post hook
+ * @param  {string}       type     action type
+ * @param  {function}     hook     hook
+ * @return {false|symbol}          return hook id or false when failed
+ */
+export const registerHook = (position, type, hook) => {
+  if (typeof hook !== 'function') {
+    return false
   }
+  if (typeof type !== 'string') {
+    return false
+  }
+  if (typeof position !== 'string') {
+    return false
+  }
+
+  if (!hooks[type]) {
+    hooks[type] = []
+  }
+
+  const id = Symbol(type)
+  hooks[type].push({ position, id, hook })
+
+  return id
+}
+
+/**
+ * curried hooking function
+ * @param  {string}   type action type
+ * @param  {function} hook hook
+ * @return {function}      curried register function
+ */
+export const registerPrehook = (type, hook) => {
+  return registerHook('pre', type, hook)
+}
+
+/**
+* curried hooking function
+ * @param  {string}   type action type
+ * @param  {function} hook hook
+ * @return {function}      curried register function
+ */
+export const registerPosthook = (type, hook) => {
+  return registerHook('post', type, hook)
+}
+
+/**
+ * unregister dispatch hook
+ * @param  {symbol} id given hook
+ * @return {void}
+ */
+export const unregisterHook = id => {
+  Object.keys(hooks).forEach(type => {
+    hooks[type] = hooks[type].filter(x => x.id !== id)
+  })
+}
+
+/**
+ * clear all hooks
+ * @return {void}
+ */
+export const clearHooks = () => {
+  Object.keys(hooks).forEach(type => {
+    delete hooks[type]
+  })
 }
 
 /**
@@ -17,10 +79,21 @@ const posthooks = {
  * @return {void}
  */
 export default store => next => action => {
-  const prehook = prehooks[action.type]
-  const posthook = posthooks[action.type]
 
-  prehook && prehook(store, action)
+  /**
+   * [getTheHooksAt description]
+   * @param  {'pre'|'post'}     position determine pre or post
+   * @return {array<function>}           hooks
+   */
+  const getTheHooksAt = position => hooks[action.type] ? (
+    hooks[action.type]
+      .filter(x => x.position === position)
+      .map(x => x.hook)
+  ) : []
+  const prehooks  = getTheHooksAt('pre')
+  const posthooks =  getTheHooksAt('post')
+
+  prehooks.forEach(hook => hook(store, action))
   next(action)
-  posthook && posthook(store, action)
+  posthooks.forEach(hook => hook(store, action))
 }
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -2,16 +2,51 @@
  * testing library
  */
 import { expect } from 'chai'
+import sinon      from 'sinon'
 
 /**
  * test target
  */
-import hookMiddleware from '../../src/index'
+import hookMiddleware, {
+  hooks,
+  registerHook,
+  unregisterHook,
+  clearHooks,
+  registerPrehook,
+  registerPosthook,
+} from '../../src/index'
+
+describe('registration functions', () => {
+
+  it(' should register, unregister and clear a hook / hooks', () => {
+    const id = registerHook('pre', 'have lunch', () => 'wash hands')
+    expect(hooks['have lunch'][0].position).to.equal('pre')
+    expect(hooks['have lunch'][0].hook()).to.equal('wash hands')
+    unregisterHook(id)
+    expect(hooks).to.deep.equal({ 'have lunch': [] })
+    clearHooks()
+    expect(hooks).to.deep.equal({})
+  })
+
+  it('should be a register prehook alias', () => {
+    registerPrehook('have lunch', () => 'wash hands')
+    expect(hooks['have lunch'][0].position).to.equal('pre')
+    expect(hooks['have lunch'][0].hook()).to.equal('wash hands')
+    clearHooks()
+  })
+
+  it('should be a register pposthook alias', () => {
+    registerPosthook('have dinner', () => 'have digestif')
+    expect(hooks['have dinner'][0].position).to.equal('post')
+    expect(hooks['have dinner'][0].hook()).to.equal('have digestif')
+    clearHooks()
+  })
+})
 
 describe('middleware', () => {
-
-  it('is a redux middleware', () => {
+  it('should be a redux middleware', () => {
     const store = {}
+    // eslint-disable-next-line require-jsdoc
     const next = () => {}
     const action = { type: 'type' }
 
@@ -19,6 +54,28 @@ describe('middleware', () => {
     expect(hookMiddleware(store)).is.a('function')
     expect(hookMiddleware(store)(next)).is.a('function')
     expect(hookMiddleware(store)(next)(action)).is.not.a('function')
+  })
+
+  it('execute each hooks', () => {
+    const store = {}
+    // eslint-disable-next-line require-jsdoc
+    const next = sinon.spy()
+    const action = { type: 'type' }
+
+    const pre1 = sinon.spy()
+    const pre2 = sinon.spy()
+    const post1 = sinon.spy()
+    const post2 = sinon.spy()
+    registerPrehook('type', pre1)
+    registerPrehook('type', pre2)
+    registerPosthook('type', post1)
+    registerPosthook('type', post2)
+    hookMiddleware(store)(next)(action)
+
+    expect(pre1.calledBefore(next)).to.be.true
+    expect(pre2.calledBefore(next)).to.be.true
+    expect(post1.calledAfter(next)).to.be.true
+    expect(post2.calledAfter(next)).to.be.true
 
   })
 })
